8b73152911d48aa226b3d6547b8ac22af81f2bfb
Mamba240824 <leo.ramos70@yahoo.com>
2023-12-04 09:16:20 -0800

    completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -9,28 +9,28 @@
 // --------------------1) What will this log?
 
 const cohort = "India 2023"
-// console.log(cohort.split(" "))
+console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The output will be the constant named cohort "split" and dislay `Inda` `2023`
+// b) Verify and explain: The output was as predicted. The split "mehtod" was used to seperate the string and achieve the predicted output. What is iteration?
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+ console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:The ouput of the code above will be udnefined since it does not have a return statemnet 
+// b) Verify and explain:The output was indeed "undefined" since it did not have a return statemnt letting the computer know what to ouput when calling upon that function. 
 
 // --------------------3) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:The output will be 11, 13, 15.
+// b) Verify and explain:The output was the odd numbers in the code, .filter being used for extracting specific elements from an array, in this case, the odd numbers. 
 
 // --------------------4) What will this log?
 
@@ -40,10 +40,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:The code above will log "Javascript"
+// b) Verify and explain:The output was "Javascript" and I was able to determine that based on .notation and I just followed the path. 0 being the first postion in the index, it also happened to be where "Javascript" was, hence the output. 
 
 // --------------------5) What will this log?
 
@@ -55,7 +55,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+ console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:The output will be George, India, 2023
+// b) Verify and explain:The ouput was George, India, and 2023 but "student", "cohort", and "year" were also listed prior to each of the previos 3 all inside curly brackets. When we console log "learn students", we call upon the instance which will call upon the class that already had 2 default values in "cohort" and "year". The name was left as a parameter until "George" was passed as the arguement resulting in that being the output for "Student:"
